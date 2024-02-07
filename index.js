0895b9e008e4934aad13b8de1696d8bef1445d54
Aerynnnn <159232615+Aerynnnn@users.noreply.github.com>
2024-02-07 14:47:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching H2O Cart","Listening to Spotify","Owner : 애 린"];
 
 
 let currentIndex = 0;
